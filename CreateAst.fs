2df36c999388f0f93d8e92bdbad66069fb355894
Dave Thomas <7sharp9@users.noreply.github.com>
2018-05-04 12:55:53 +0100

    Removed unused code.

--- CreateAst.fs
+++ CreateAst.fs
@@ -7,43 +7,6 @@ open Microsoft.FSharp.Compiler.Ast
 open Microsoft.FSharp.Compiler.Range
 open Microsoft.FSharp.Quotations
 
-//todo: move to resting place
-[<RequireQualifiedAccess>]
-module Ensure =
-    let argNotNull name (value: obj) =
-        if isNull value
-        then raise <| ArgumentNullException(name)
-        
-[<RequireQualifiedAccess>]
-module Naming =
-    
-    let private mapFirstChar f (input: string) =
-        if String.IsNullOrEmpty input then input
-        else
-            let first = input.[0] |> f |> string
-            if input.Length > 1 then first + input.[1..]
-            else first
-    
-    /// Converts "name_like_that" to "NameLikeThat"
-    let snakeToPascal (identifier: string) =
-        identifier.Split('_')
-        |> Seq.map (mapFirstChar Char.ToUpper)
-        |> String.concat String.Empty
-    
-    /// Converts "NameLikeThat" to "nameLikeThat"
-    let pascalToCamel = mapFirstChar Char.ToLower
-    
-    /// Converts "nameLikeThat" to "NameLikeThat" 
-    let camelToPascal = mapFirstChar Char.ToUpper
-    
-    /// Converts "name_like_that" to "nameLikeThat"
-    let snakeToCamel = snakeToPascal >> pascalToCamel
-    
-    /// Converts "NAME_LIKE_THAT" to "nameLikeThat"
-    let upperSnakeToPascal (identifier: string) =
-        snakeToPascal <| identifier.ToLower()
-
-
 let createBasicClass() =
     let mdl = "BasicClass"
 
