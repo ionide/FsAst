b4b26540c45c5abeb68649a21c409dbd1618859c
Cameron Taggart <cameron.taggart@gmail.com>
2018-10-01 23:40:14 -0500

    publish a nupkg (#9)

---
+++ build.ps1
@@ -0,0 +1,20 @@
+$version = '0.1.0' # the version under development, update after a release
+$versionSuffix = '-build.0' # manually incremented for local builds
+
+function isVersionTag($tag){
+    $v = New-Object Version
+    [Version]::TryParse($tag, [ref]$v)
+}
+
+if ($env:appveyor){
+    $versionSuffix = '-build.' + $env:appveyor_build_number
+    if ($env:appveyor_repo_tag -eq 'true' -and (isVersionTag($env:appveyor_repo_tag_name))){
+        $version = $env:appveyor_repo_tag_name
+        $versionSuffix = ''
+    }
+    Update-AppveyorBuild -Version "$version$versionSuffix"
+}
+
+dotnet build -c Release FsAst.sln /p:Version=$version$versionSuffix
+# dotnet test --no-build -c Release FsAst.Test.fsproj
+dotnet pack --no-build -c Release FsAst.fsproj /p:Version=$version$versionSuffix -o $psscriptroot/bin
