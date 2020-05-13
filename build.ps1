8b4e7797ab52daf15ba447829f77ae6ac93f94c2
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-05-13 12:40:13 +0200

    Bump version to 0.2.1

--- build.ps1
+++ build.ps1
@@ -1,20 +1,6 @@
-$version = '0.2.0' # the version under development, update after a release
-$versionSuffix = '-build.0' # manually incremented for local builds
+$version = '0.2.1' # the version under development, update after a release
 
-function isVersionTag($tag){
-    $v = New-Object Version
-    [Version]::TryParse($tag, [ref]$v)
-}
-
-if ($env:appveyor){
-    $versionSuffix = '-build.' + $env:appveyor_build_number
-    if ($env:appveyor_repo_tag -eq 'true' -and (isVersionTag($env:appveyor_repo_tag_name))){
-        $version = $env:appveyor_repo_tag_name
-        $versionSuffix = ''
-    }
-    Update-AppveyorBuild -Version "$version$versionSuffix"
-}
-
-dotnet build -c Release FsAst.sln /p:Version=$version$versionSuffix
+dotnet tool restore
+dotnet build -c Release FsAst.sln /p:Version=$version
 # dotnet test --no-build -c Release FsAst.Test.fsproj
-dotnet pack --no-build -c Release FsAst.fsproj /p:Version=$version$versionSuffix -o $psscriptroot/bin
+dotnet pack --no-build -c Release src/FsAst.fsproj /p:Version=$version -o $psscriptroot/bin
