262246a376f158529773670f3c523b3c61f07179
Zaid Ajaj <Zaid-Ajaj@users.noreply.github.com>
2021-04-29 11:01:15 +0200

    Added missing types into SynType and fixed the sample code (#28)

--- sample/Program.fs
+++ sample/Program.fs
@@ -3,25 +3,11 @@
 [<EntryPoint>]
 let main argv =
 
-    let fs =
-       __SOURCE_DIRECTORY__ +
-        @"/Hello.fs"
-//        @"/Enum.fs"
-//        @"/PInvoke.fs"
-
-    let checker = Fantomas.FakeHelpers.sharedChecker.Value
-
-    printfn "### printAstInfo\n"
-    PrintAstInfo.printAstInfo fs checker
-    |> Async.RunSynchronously
-
-    printfn "\n### formatFs:\n"
-    FormatFs.formatFs fs checker
-    |> Async.RunSynchronously
-
     printfn "\n### create AST:\n"
-    CreateAst.createBasicClass()
-    |> Async.RunSynchronously
-//    CreateAst.createBasicEnums()
-//    CreateAst.createBasicPInvoke()
+    [
+        CreateAst.createBasicClass
+        CreateAst.createBasicEnums
+        CreateAst.createBasicPInvoke
+    ]
+    |> List.iter (fun task -> Async.RunSynchronously(task()))
     0
